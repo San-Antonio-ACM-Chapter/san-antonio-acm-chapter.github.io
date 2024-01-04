5c95a4b2ac8853f1cde4ca499bca59f26aa79c71
Gene <33785659+Geneonosis@users.noreply.github.com>
2024-01-03 23:55:03 -0600

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,37 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import NavigationHeader from './assets/Components/organisms/NavigationHeader/NavigationHeader';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+          <NavigationHeader/>
+          <div className={"marque-plate"}>
+              <div className={"marque-container"}/>
+
+          </div>
+          <div className={"hover-container"}>
+              <h1>Software Engineering ACM San Antonio</h1>
+              <p>under construction</p>
+          </div>
+          <section>
+              <div>
+                  <h2>What is ACM?</h2>
+                  <p>
+                    lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  </p>
+              </div>
+          </section>
+          <section>
+              <div>
+                  <h2>Sponsors</h2>
+                  <p>
+                      lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  </p>
+              </div>
+          </section>
+      </div>
   );
 }
 
