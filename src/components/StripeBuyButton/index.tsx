cb3e88994d427c038e5c310027ac355e327add80
Zaquariah Holland <zaquariah@gmail.com>
2025-02-23 22:44:08 -0600

    chore: cleanup

--- src/components/StripeBuyButton/index.tsx
+++ src/components/StripeBuyButton/index.tsx
@@ -1,10 +1,4 @@
-import { useEffect } from "react";
-
 const StripeBuyButton = () => {
-  useEffect(() => {
-    // The script should be loaded, and the element should render
-  }, []);
-
   return (
     <stripe-buy-button
       buy-button-id="buy_btn_1QvmDQDmV4iVmxQ2zMXbmBgH"
