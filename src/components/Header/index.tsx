836a8db74f02b6a10f1ad2d89cf381a413d02143
Zaquariah Holland <zaquariah@gmail.com>
2025-02-24 21:47:29 -0600

    feat: Updates & adding Header folder

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,22 @@
+import styles from './styles.module.css';
+import NavigationLink from './navigationLink';
+
+const Header = () => {
+    return (
+        <div className={styles.container}>
+            <span className={styles.spanContainer}>
+                <h2 className={styles.headerMainText}>
+                    <NavigationLink href={"#home"} text={"ACM San Antonio"} />
+                </h2>
+                <span className={styles.linkContainer}>
+                    <NavigationLink href={"#about"} text={"About"} />
+                    <NavigationLink href={"#events"} text={"Events"} />
+                    <NavigationLink href={"#membership"} text={"Membership"} />
+                    <NavigationLink href={"#contact"} text={"Connect"} />
+                </span>
+            </span>
+        </div>
+    );
+}
+
+export default Header;
