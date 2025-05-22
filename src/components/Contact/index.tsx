ce238b0a77bf063ad3bcd1cd6f3cff2af6b1fe30
Zaquariah Holland <zaquariah@gmail.com>
2025-05-21 22:43:18 -0500

    feat: modified content section for footer (#21)

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -10,28 +10,37 @@ const meetupPageURL: string = "https://www.meetup.com/acm-sa/";
 
 const Contact = () => {
     return (
-        <div className={styles.contactSection}>
-            <h1>Connect With Us</h1>
-            <div className={styles.socialMediaContainer}>
-                {/* discord icon */}
-                <a href={discordInviteURL} target="_blank" rel="noreferrer">
-                    <img src={socials?.discord?.logo} alt="discord logo" />
-                </a>
-                <SocialLink
-                    link={socials?.instagram?.url}
-                    src={ContactUsIcons.INSTAGRAM}
-                    title={socials?.instagram?.title}
-                />
-                <SocialLink
-                    link={socials?.linkedin?.url}
-                    src={ContactUsIcons.LINKEDIN}
-                    title={socials?.linkedin?.title}
-                />
-                <a href={meetupPageURL} target="_blank" rel="noreferrer">
-                    <img src={meetupLogo} alt="meetup logo" />
-                </a>
+        <footer className={styles.contact}>
+            <div className={styles.contactSection}>
+                <h1>Connect With Us</h1>
+                <div className={styles.socialMediaContainer}>
+                    {/* discord icon */}
+                    <a href={discordInviteURL} target="_blank" rel="noreferrer">
+                        <img src={socials?.discord?.logo} alt="discord logo" />
+                    </a>
+                    <SocialLink
+                        link={socials?.instagram?.url}
+                        src={ContactUsIcons.INSTAGRAM}
+                        title={socials?.instagram?.title}
+                    />
+                    <SocialLink
+                        link={socials?.linkedin?.url}
+                        src={ContactUsIcons.LINKEDIN}
+                        title={socials?.linkedin?.title}
+                    />
+                    <a href={meetupPageURL} target="_blank" rel="noreferrer">
+                        <img src={meetupLogo} alt="meetup logo" />
+                    </a>
+                </div>
             </div>
-        </div>
+            <div className={styles.simpleContact}>
+                <p>
+                    2025 nonprofit
+                    &#8226;
+                    <a href='mailto:team@acmsa.org'>team@acmsa.org</a>
+                </p>
+            </div>
+        </footer>
     )
 }
 
